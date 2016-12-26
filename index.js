8f5d6bab1e60cd0cd7148c3d305a752cbe015058
Chris Broome <christopher.broome@neustar.biz>
2016-12-26 16:00:16 -0500

    Convert to es6 module

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-exports.createFileInfo = require('./lib/file-info');
-exports.createToken = require('./lib/token');
-exports.createTokenizer = require('./lib/tokenizer');
-exports.createTerminal = require('./lib/terminal');
-exports.createTerminalList = require('./lib/terminal-list');
-exports.createTerminalMatch = require('./lib/terminal-match');
-exports.createTerminalMatchList = require('./lib/terminal-match-list');
+import createFileInfo from './src/file-info'
+import createToken from './src/token'
+import createTokenizer from './src/tokenizer'
+import createTerminal from './src/terminal'
+import createTerminalList from './src/terminal-list'
+import createTerminalMatch from './src/terminal-match'
+import createTerminalMatchList from './src/terminal-match-list'
+
+export default {
+  createFileInfo,
+  createToken,
+  createTokenizer,
+  createTerminal,
+  createTerminalList,
+  createTerminalMatch,
+  createTerminalMatchList,
+}
